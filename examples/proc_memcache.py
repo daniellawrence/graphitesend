9ef1b8a0235bc72834c83045d4afddd7bbcae9c3
daniellawrence <dannyla@linux.com>
2013-04-05 23:56:32 +1100

    added gather for memcache

---
+++ examples/proc_memcache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import graphitesend                             
+ 
+g = graphitesend.init(group='meminfo.', suffix='_mb', lowercase_metric_names=True)
+data = []
+for line in  open('/proc/meminfo').readlines():
+    bits = line.split()
+
+    # We dont care about the pages.
+    if len(bits) == 2:
+        continue
+
+    # remove the : from the metric name
+    metric = bits[0]
+    metric = metric.replace(':','')
+
+    # Covert the default kb into mb
+    value = int(bits[1])
+    value = value / 1024
+
+    data.append((metric, value))
+
+print g.send_list(data)
