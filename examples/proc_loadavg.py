1be6daee9156eccf4b0a861f8b5283992f864fec
daniellawrence <dannyla@linux.com>
2013-04-05 21:50:03 +1100

    added 2 examples from blog post from dansysadm.com

---
+++ examples/proc_loadavg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import time   
+import graphitesend                             
+ 
+g = graphitesend.init(group='loadavg_', suffix='min')
+while True:
+    (la1, la5, la15 ) = open('/proc/loadavg').read().strip().split()[:3]
+    print g.send_dict( {'1': la1, '5': la5, '15': la15} )
+    time.sleep(1)
---
+++ examples/proc_net_netstat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import graphitesend
+
+lines = open('/proc/net/netstat').readlines()
+
+tcp_metrics = lines[0].split()[1:]
+tcp_values = lines[1].split()[1:]
+ip_metrics = lines[2].split()[1:]
+ip_values = lines[3].split()[1:]
+
+data_list = zip( tcp_metrics + ip_metrics, tcp_values + ip_values )
+
+g = graphitesend.init(group='netstat.', lowercase_metric_names=True)
+print g.send_list( data_list )
+
