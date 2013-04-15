effc41b77a2231c17f3bf1e69ff8da73cc40ebff
Daniel Lawrence <dannyla@linux.com>
2013-04-15 18:17:51 +1000

    pep8

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md') as file:
         long_description = file.read()
 
 setup(
-   name = 'graphitesend',
-   version = '0.0.5',
-   description = 'A simple interface for sending metrics to Graphite',
-   author = 'Danny Lawrence',
-   author_email = 'dannyla@linux.com',
-   url = 'https://github.com/daniellawrence/graphitesend',
-   package_dir = {'': 'src'},
-   packages = [''],
-   scripts = ['bin/graphitesend'],
-   long_description = long_description
- )
+    name='graphitesend',
+    version='0.0.5',
+    description='A simple interface for sending metrics to Graphite',
+    author='Danny Lawrence',
+    author_email='dannyla@linux.com',
+    url='https://github.com/daniellawrence/graphitesend',
+    package_dir={'': 'src'},
+    packages=[''],
+    scripts=['bin/graphitesend'],
+    long_description=long_description
+)
